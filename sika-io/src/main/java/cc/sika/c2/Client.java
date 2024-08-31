a9e1f0707c27e035134e70aa7026982fd86abe43
spirit223 <2235569493@qq.com>
2024-08-31 14:59:06 +0800

    服务器测试代码

--- sika-io/src/main/java/cc/sika/c2/Client.java
+++ sika-io/src/main/java/cc/sika/c2/Client.java
@@ -1,16 +1,48 @@
 package cc.sika.c2;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
 
 /**
  * NIO 服务器实例的演示客户端
  */
+@Slf4j
 public class Client {
+    private static final InetSocketAddress SOCKET_ADDRESS = new InetSocketAddress("127.0.0.1", 8088);
     public static void main(String[] args) throws IOException {
-        SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.connect(new InetSocketAddress("127.0.0.1", 8088));
-        System.out.println("waiting...");
+
+        try (SocketChannel socketChannel = SocketChannel.open()) {
+            socketChannel.connect(SOCKET_ADDRESS);
+            log.info("connected server!");
+
+            // simulated transmission data
+            for (int i = 1; i <= 10; i++) {
+                // send
+                String clientMessage = String.format("message%d\n", i);
+                socketChannel.write(StandardCharsets.UTF_8.encode(clientMessage));
+                log.debug("send message: {}", clientMessage);
+
+                // accept ack message
+                ByteBuffer ackBuffer = ByteBuffer.allocate(1024);
+                int readLength = socketChannel.read(ackBuffer);
+                if (readLength > 0) {
+                    ackBuffer.flip();
+                    String response = StandardCharsets.UTF_8.decode(ackBuffer).toString();
+                    log.info("response: {}", response);
+                }
+                ackBuffer.clear();
+                // sleep
+                log.info("sleep...");
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
