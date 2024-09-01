ba3df6239bdcc8025bef2b1309ca16cd9653bdae
spirit223 <2235569493@qq.com>
2024-09-01 15:27:48 +0800

    todo

--- sika-io/src/main/java/cc/sika/c2/BlockServerThreadPool.java
+++ sika-io/src/main/java/cc/sika/c2/BlockServerThreadPool.java
@@ -2,6 +2,12 @@ package cc.sika.c2;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * <p>Blocking Server by NIO</p>
  * <p>process client request with independence thread form thread pool</p>
@@ -13,9 +19,24 @@ public class BlockServerThreadPool {
     public static void main(String[] args) {
         // create thread pool, allocate some thread
         // which is the best choice not to use fixedThreadPoolExecutor?
+        try(ThreadPoolExecutor pool = new ThreadPoolExecutor(10, 20,
+                60L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(100), new SikaTreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy());) {
+
+        }
 
         // accept client connect
 
         // process request with independence thread in pool
     }
+
+    static class SikaTreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        @Override
+        public Thread newThread(Runnable task) {
+            return new Thread(task, "Custom Thread #" + poolNumber.getAndIncrement());
+        }
+    }
+
 }
