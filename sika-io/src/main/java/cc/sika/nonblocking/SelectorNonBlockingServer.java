b44ede9840bb87a3266613ca2caa1a76d0fb3941
spirit223 <2235569493@qq.com>
2024-09-03 18:06:30 +0800

    bug fixed

--- sika-io/src/main/java/cc/sika/nonblocking/SelectorNonBlockingServer.java
+++ sika-io/src/main/java/cc/sika/nonblocking/SelectorNonBlockingServer.java
@@ -10,6 +10,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -67,10 +68,26 @@ public class SelectorNonBlockingServer {
                         client.configureBlocking(false);
                         // register new channel to selector,
                         // this is client(socket channel) so interesting in READ / WRITE event
-                        client.register(selector, SelectionKey.OP_READ);
-                    } else if (event.isReadable()) {
-                        SocketChannel client = (SocketChannel) event.channel();
+                        // add byte buffer as channel attachment
                         ByteBuffer buffer = ByteBuffer.allocate(16);
+                        client.register(selector, SelectionKey.OP_READ, buffer);
+                        // interest write and send connection success message
+                        ByteBuffer successMessageBuffer = StandardCharsets.UTF_8.encode("connect to server is success");
+                        int writeLength = client.write(successMessageBuffer);
+                        // if server haven't finished writing all the data at once,
+                        // interest in OP_WRITE to prepare for write again
+                        // use while will break non-blocking
+                        if (successMessageBuffer.hasRemaining()) {
+                            event.interestOps(event.interestOps() | SelectionKey.OP_WRITE);
+                            // transfer buffer to next write operation
+                            event.attach(successMessageBuffer);
+                        }
+
+                    }
+                    else if (event.isReadable()) {
+                        SocketChannel client = (SocketChannel) event.channel();
+                        // get buffer from key
+                        ByteBuffer buffer = (ByteBuffer) event.attachment();
                         try {
                             int readLength = client.read(buffer);
                             // when client close or shutdown, it will trigger READ event without data
@@ -78,15 +95,36 @@ public class SelectorNonBlockingServer {
                                 throw new EOFException();
                             }
                             else {
-                                buffer.flip();
-                                debugAll(buffer);
+                                split(buffer);
+                                // after compact, position equals to limit,
+                                // certificate message larger than buffer, dilatation buffer
+                                if (buffer.position() == buffer.limit()) {
+                                    ByteBuffer newBuffer = ByteBuffer.allocate(buffer.capacity() << 1);
+                                    // transfer data of buffer to new buffer
+                                    buffer.flip();
+                                    newBuffer.put(buffer);
+                                    event.attach(newBuffer);
+
+                                }
                             }
                         } catch (IOException e) {
                             log.info("client unlined! ");
                             event.cancel();
                             client.close();
                         }
-                    } else {
+                    }
+                    else if (event.isWritable()) {
+                        ByteBuffer attachment = (ByteBuffer) event.attachment();
+                        SocketChannel client = (SocketChannel) event.channel();
+                        client.write(attachment);
+                        // finished writing data, cancel interest in OP_WRITE
+                        if (!attachment.hasRemaining()) {
+                            event.attach(null);
+//                            event.interestOps(event.interestOps() & ~SelectionKey.OP_WRITE);
+                            event.interestOps(event.interestOps() ^ SelectionKey.OP_WRITE);
+                        }
+                    }
+                    else {
                         // if you don't do anything, make sure you invoke key's cancel method to consume event
                         // avoid unlimited loop
                         event.cancel();
@@ -95,6 +133,36 @@ public class SelectorNonBlockingServer {
             }
         } catch (IOException ioException) {
             log.error("connection error: ", ioException);
+        }
+    }
+
+    /**
+     * split complete message by new line('\n')
+     * if split slower than message send, buffer.compact() maybe cause lose data
+     * because buffer too small
+     * @param buffer data buffer from server to receive client data
+     */
+    private static void split(ByteBuffer buffer) {
+        assertReadable(buffer);
+        for (int i = 0; i < buffer.limit(); i++) {
+            // 一条消息结束, 拆分后放入一个新的 ByteBuffer
+            if (buffer.get(i) == '\n') {
+                int size = i + 1 - buffer.position();
+                ByteBuffer target = ByteBuffer.allocate(size);
+                for (int j = 0; j < size; j++) {
+                    target.put(buffer.get());
+                }
+                target.flip();
+//                log.info("split complete message is ==> [{}]", StandardCharsets.UTF_8.decode(target));
+                debugAll(target);
+            }
+        }
+        buffer.compact();
+    }
+
+    private static void assertReadable(ByteBuffer buffer) {
+        if (!(buffer.limit() == buffer.capacity() && buffer.position() == 0)) {
+            buffer.flip();
         }
     }
 }
